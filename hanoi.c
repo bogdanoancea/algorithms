478abffb7ecf6425027e5b3b6108c4b2dc882c1e
bogdanoancea <bogdan.oancea@gmail.com>
2019-05-18 15:48:16 +0300

    hanoi

---
+++ hanoi.c
@@ -0,0 +1,29 @@
+/* Problema Turnurilor din Hanoi - Metoda Divide et Impera
+Se dau 3 tije A,B,C pe tija A aflandu-se n discuri de dimensiuni
+diferite, aranjate de la baza spre varf de la cel mai la cel mai 
+mic.Stiind ca se muta cate un disc la un moment dat si ca nu se
+poate muta un disc mai mare peste un disc mai mic,sa se mute toate
+cele n discuri de pe tija A pe tija B utilizand drept ajutor tija C.*/
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+
+void hanoi(int nr_discuri,int start,int stop,int ajutor)
+{
+	if(nr_discuri == 1)
+		printf("Muta un disc de pe tija %d pe tija %d\n",start,stop);
+	  else
+	   {
+		  hanoi(nr_discuri-1,start,ajutor,stop);
+		  printf("Muta un disc de pe tija %d pe tija %d\n",start,stop);
+		  hanoi(nr_discuri-1,ajutor,start,stop);
+	   }
+}
+
+int main()
+ {
+	 int n;
+	 printf("Dati nr de discuri:");scanf("%d",&n);
+	 hanoi(n,1,2,3);
+     return 0;
+ }
